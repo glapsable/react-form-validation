409b1b8b035436b7355705ca1eb91d31f52249fb
Glapson <glapsable@gmail.com>
2019-02-28 19:12:06 +0100

    setup coordinator validation part 1

--- src/components/AddEventPage.js
+++ src/components/AddEventPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
+// import PropTypes from 'prop-types';
 import categories from '../mocks/categories';
 import coordinators from '../mocks/employes';
 
@@ -41,17 +42,46 @@ class AddEventPage extends React.Component {
       id: '',
       email: '',
     },
+    dateTime: {
+      date: '',
+      time: '',
+      am: true,
+    },
+    duration: '',
+    errors: {
+      title: false,
+      description: false,
+      coordinator: false,
+    },
   };
 
   onTitleChange = (e) => {
-    const title = e.target.value;
+    const title = e.target.value.trim();
+    const { errors } = this.state;
     this.setState(() => ({ title }));
+    if (errors.title === true) {
+      this.setState(prevState => ({
+        errors: {
+          ...prevState.errors,
+          title: false,
+        },
+      }));
+    }
   };
 
   onDescriptionChange = (e) => {
     const description = e.target.value;
+    const { errors } = this.state;
     if (description.length <= 140) {
       this.setState(() => ({ description }));
+    }
+    if (errors.description === true) {
+      this.setState(prevState => ({
+        errors: {
+          ...prevState.errors,
+          description: false,
+        },
+      }));
     }
   };
 
@@ -94,15 +124,89 @@ class AddEventPage extends React.Component {
 
   onEmailChange = (e) => {
     const email = e.target.value;
-    this.setState(() => ({
+    this.setState(prevState => ({
       coordinator: {
+        ...prevState.coordinator,
         email,
       },
     }));
   };
 
+  onDateChange = (e) => {
+    const date = e.target.value;
+    this.setState(prevState => ({
+      dateTime: {
+        ...prevState.dateTime,
+        date,
+      },
+    }));
+  };
+
+  onTimeChange = (e) => {
+    const time = e.target.value;
+    this.setState(prevState => ({
+      dateTime: {
+        ...prevState.dateTime,
+        time,
+      },
+    }));
+  };
+
+  onAmPmChange = (e) => {
+    const amPm = e.target.name;
+    if (amPm === 'am') {
+      this.setState(prevState => ({
+        dateTime: {
+          ...prevState.dateTime,
+          am: true,
+        },
+      }));
+    } else if (amPm === 'pm') {
+      this.setState(prevState => ({
+        dateTime: {
+          ...prevState.dateTime,
+          am: false,
+        },
+      }));
+    }
+  };
+
+  onDurationChange = (e) => {
+    const duration = e.target.value;
+    if (duration.match(/^[0-9]*$/)) {
+      this.setState({
+        duration,
+      });
+    }
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
+    const { title, description, coordinator } = this.state;
+    if (!title.trim()) {
+      this.setState(prevState => ({
+        errors: {
+          ...prevState.errors,
+          title: true,
+        },
+      }));
+    }
+    if (!description.trim()) {
+      this.setState(prevState => ({
+        errors: {
+          ...prevState.errors,
+          description: true,
+        },
+      }));
+    }
+    if (!coordinator.id) {
+      this.setState(prevState => ({
+        errors: {
+          ...prevState.errors,
+          coordinator: true,
+        },
+      }));
+    }
   };
 
   render() {
@@ -114,6 +218,9 @@ class AddEventPage extends React.Component {
       eventFee,
       reward,
       coordinator,
+      dateTime,
+      duration,
+      errors,
     } = this.state;
 
     return (
@@ -124,13 +231,13 @@ class AddEventPage extends React.Component {
           <div className="input-group">
             <label htmlFor="title">
               Title
-              <input type="text" value={title} onChange={this.onTitleChange} id="title" />
+              <input type="text" className={errors.title === true ? 'error' : ''} value={title} onChange={this.onTitleChange} id="title" />
             </label>
           </div>
           <div className="input-group">
             <label htmlFor="description">
               Description
-              <textarea value={description} onChange={this.onDescriptionChange} id="description" />
+              <textarea className={errors.description === true ? 'error' : ''} value={description} onChange={this.onDescriptionChange} id="description" />
             </label>
             <p>{description.length}/140</p>
           </div>
@@ -184,13 +291,39 @@ class AddEventPage extends React.Component {
             <input type="text" value={coordinator.email} onChange={this.onEmailChange} />
           </div>
         </div>
-        <div className="input-group">
+        <div>
           <h2>When</h2>
           <hr />
+          <div className="input-group">
+            Starts on*
+            <input type="date" value={dateTime.date} onChange={this.onDateChange} />
+            <div className="input-group__inner-group">
+              at <input type="time" min="00:00" max="12:00" value={dateTime.time} onChange={this.onTimeChange} />
+              <label htmlFor="am">
+                AM
+                <input type="radio" checked={dateTime.am} onChange={this.onAmPmChange} id="am" name="am" />
+              </label>
+              <label htmlFor="pm">
+                PM
+                <input type="radio" checked={!dateTime.am} onChange={this.onAmPmChange} id="pm" name="pm" />
+              </label>
+            </div>
+          </div>
+          <div className="input-group">
+            Duration
+            <div className="input-group__inner-group">
+              <input type="text" value={duration} onChange={this.onDurationChange} placeholder="Number" /> hour
+            </div>
+          </div>
         </div>
+        <button type="submit">Publish event</button>
       </form>
     );
   }
 }
+
+// AddEventPage.propTypes = {
+//   history: PropTypes.func.isRequired,
+// };
 
 export default AddEventPage;
